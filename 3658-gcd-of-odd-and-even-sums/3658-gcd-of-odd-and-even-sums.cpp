c45e20a291ef61d0652b8d488713caa60947b8a2
Sheetal Pareek <rajeshpareeksheetal@gmail.com>
2026-07-16 01:23:00 +0530

    Time: 0 ms (100%), Space: 8.6 MB (9.28%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
@@ -1,22 +1,6 @@
 class Solution {
-public: 
-    int solve(int a,int b){
-        while(b!=0){
-            int rem=a%b;
-            a=b;
-            b=rem;
-        }
-        return a;
-    }
+public:
     int gcdOfOddEvenSums(int n) {
-        int oddsum=0;
-        int evensum=0;
-        for(int i=1;i<2*n;i+=2){
-            oddsum+=i;
-        }
-        for(int i=2;i<2*n+1;i+=2){
-            evensum+=i;
-        }
-       return solve(oddsum,evensum);
+        return n;
     }
 };
