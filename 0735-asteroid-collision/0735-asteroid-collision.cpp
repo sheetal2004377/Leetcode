23c368d79c0af77861e2a0fa81744eaaa431e70e
Sheetal Pareek <rajeshpareeksheetal@gmail.com>
2026-07-16 20:08:43 +0530

    Time: 4 ms (14.75%), Space: 21.6 MB (89.9%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        int n=asteroids.size();
+        vector<int>st;
+        for(int i=0;i<n;i++){
+            if(asteroids[i]>0){
+                st.push_back(asteroids[i]);
+            }
+            else{
+                
+                while(!st.empty()&&st.back()>0&&abs(st.back())<abs(asteroids[i])){
+                    st.pop_back();
+                }
+                 if(st.empty()||st.back()<0){
+                    st.push_back(asteroids[i]);
+                }
+               
+                 else if(!st.empty()&&abs(st.back())==abs(asteroids[i])){
+                    st.pop_back();
+                }
+               
+            }
+        }
+        return st;
+        
+    }
+};
