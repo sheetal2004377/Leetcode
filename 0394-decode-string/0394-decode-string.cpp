ef9f51efed63edb82e80ad80e88e35f7148aa7df
Sheetal Pareek <rajeshpareeksheetal@gmail.com>
2026-07-17 17:34:30 +0530

    Time: 2 ms (24.21%), Space: 9.3 MB (65.1%) - LeetHub

--- 0394-decode-string/0394-decode-string.cpp
+++ 0394-decode-string/0394-decode-string.cpp
@@ -1,48 +1,36 @@
 class Solution {
 public:
     string decodeString(string s) {
-        stack<int> countStack;
-        stack<string> stringStack;
-
-        string curr = "";
-        int num = 0;
-
-        for (int i = 0; i < s.size(); i++) {
-
-            if (isdigit(s[i])) {
-                num = num * 10 + (s[i] - '0');
+        int n=s.size();
+        stack<int>nums;
+        stack<string>st;
+        string ans="";
+        int num=0;
+        for(int i=0;i<n;i++){
+            if(isdigit(s[i])){
+                num=num*10+(s[i]-'0');
             }
-
-            else if (s[i] == '[') {
-                countStack.push(num);
-                stringStack.push(curr);
-
-                num = 0;
-                curr = "";
+            else if(s[i]=='['){
+                nums.push(num);
+                st.push(ans);
+                num=0;
+                ans="";
             }
-
-            else if (isalpha(s[i])) {
-                curr += s[i];
+            else if(isalpha(s[i])){
+                ans+=s[i];
             }
-
-            else if (s[i] == ']') {
-
-                int repeat = countStack.top();
-                countStack.pop();
-
-                string prev = stringStack.top();
-                stringStack.pop();
-
-                string temp = "";
-
-                while (repeat--) {
-                    temp += curr;
+            else if(s[i]==']'){
+                int repeat=nums.top();
+                nums.pop();
+                string prev=st.top();
+                st.pop();
+                string temp="";
+                while(repeat--){
+                    temp+=ans;
                 }
-
-                curr = prev + temp;
+                ans=prev+temp;
             }
         }
-
-        return curr;
+      return  ans;
     }
 };
