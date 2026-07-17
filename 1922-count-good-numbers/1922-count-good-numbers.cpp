6b948cc3fd56e22bb7fff93e2ad4c69ef423532e
Sheetal Pareek <rajeshpareeksheetal@gmail.com>
2026-07-17 20:04:51 +0530

    Time: 0 ms (100%), Space: 8.1 MB (2.3%) - LeetHub

--- 1922-count-good-numbers/1922-count-good-numbers.cpp
+++ 1922-count-good-numbers/1922-count-good-numbers.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
-    const long long MOD = 1000000007;
+    const long long MOD=1000000007;
     int power(long long x,long long n){
         if(n==0)return 1;
         long long ans=power(x,n/2);
         ans=(ans*ans)%MOD;
-        if(n%2==1)
-        ans=(ans*x)%MOD;
+        if(n%2==1)ans=(ans*x)%MOD;
         return ans;
     }
     int countGoodNumbers(long long n) {
@@ -15,6 +14,7 @@ public:
         long long even=power(5,evenways);
         long long odd=power(4,oddways);
         return (even*odd)%MOD;
+
         
     }
 };
