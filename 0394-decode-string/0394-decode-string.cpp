2340a17ed28618407c24b00d13cd37a42b74890f
Sheetal Pareek <rajeshpareeksheetal@gmail.com>
2026-07-20 02:17:23 +0530

    Time: 0 ms (100%), Space: 9.7 MB (18.34%) - LeetHub

--- 0394-decode-string/0394-decode-string.cpp
+++ 0394-decode-string/0394-decode-string.cpp
@@ -2,35 +2,36 @@ class Solution {
 public:
     string decodeString(string s) {
         int n=s.size();
-        stack<int>nums;
-        stack<string>st;
         string ans="";
         int num=0;
+        stack<int>st;
+        stack<string>alp;
         for(int i=0;i<n;i++){
             if(isdigit(s[i])){
                 num=num*10+(s[i]-'0');
             }
-            else if(s[i]=='['){
-                nums.push(num);
-                st.push(ans);
-                num=0;
-                ans="";
-            }
             else if(isalpha(s[i])){
                 ans+=s[i];
             }
+            else if(s[i]=='['){
+                alp.push(ans);
+                st.push(num);
+                num=0;      
+                ans="";
+            }
             else if(s[i]==']'){
-                int repeat=nums.top();
-                nums.pop();
-                string prev=st.top();
+                int val=st.top();
                 st.pop();
+                string prev=alp.top();
+                alp.pop();
                 string temp="";
-                while(repeat--){
-                    temp+=ans;
-                }
+                while(val>0){
+                temp+=ans;
+                val--;
+                }   
                 ans=prev+temp;
             }
         }
-      return  ans;
+        return ans;
     }
 };
