40d704babb6cade58648125e4028a624cc225b4e
Sheetal Pareek <rajeshpareeksheetal@gmail.com>
2026-07-20 01:53:47 +0530

    Time: 43 ms (80.42%), Space: 106.9 MB (27.97%) - LeetHub

---
+++ 0916-word-subsets/0916-word-subsets.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
+        vector<int>need(26,0);
+        for(string word:words2){
+            vector<int>freq(26,0);
+            for(char ch:word){
+                freq[ch-'a']++;
+            }
+            for(int i=0;i<26;i++){
+                need[i]=max(need[i],freq[i]);
+            }
+        }
+        vector<string >ans;
+        for(string word:words1){
+            vector<int>freq(26,0);
+            for(char ch:word){
+                freq[ch-'a']++;
+            }
+            bool ok=true;
+            for(int i=0;i<26;i++){
+                if(freq[i]<need[i]){
+                    ok=false;
+                    break;
+                }
+            }
+            if(ok)ans.push_back(word);
+        }
+        return ans;
+    }
+};
