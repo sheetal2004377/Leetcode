29563b4a17f4f72cb4f80b3be9f36626d3036d5e
Sheetal Pareek <rajeshpareeksheetal@gmail.com>
2026-07-24 19:11:54 +0530

    Time: 2071 ms (35.39%), Space: 98.3 MB (39.89%) - LeetHub

--- 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.cpp
+++ 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.cpp
@@ -1,36 +1,23 @@
 class Solution {
 public:
     int uniqueXorTriplets(vector<int>& nums) {
-        int m = 0;
-        for (int v : nums) {
-            m = max(m, v);
-        }
-        int u = 1;
-        while (u <= m) {
-            u <<= 1;
-        }
-        vector<int> s(u);
         int n = nums.size();
+
+        unordered_set<int> pairXor;
+        unordered_set<int> ans;
         for (int i = 0; i < n; i++) {
             for (int j = i; j < n; j++) {
-                s[nums[i] ^ nums[j]] = 1;
+                pairXor.insert(nums[i] ^ nums[j]);
             }
         }
-        vector<int> t(u);
-        for (int x = 0; x < u; x++) {
-            if (!s[x]) {
-                continue;
-            }
-            for (int v : nums) {
-                t[x ^ v] = 1;
+
+        // XOR every pair XOR with every number
+        for (int x : pairXor) {
+            for (int num : nums) {
+                ans.insert(x ^ num);
             }
         }
-        int ans = 0;
-        for (int x = 0; x < u; x++) {
-            if (t[x]) {
-                ans++;
-            }
-        }
-        return ans;
+
+        return ans.size();
     }
 };
