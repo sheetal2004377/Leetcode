fdf212c3540dc4442b6acf34b4057c76f858ba24
Sheetal Pareek <rajeshpareeksheetal@gmail.com>
2026-07-25 11:48:35 +0530

    Time: 0 ms (100%), Space: 11.8 MB (55.13%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -4,25 +4,26 @@ public:
         int n=obstacleGrid.size();
         int m=obstacleGrid[0].size();
         vector<vector<int>>dp(n,vector<int>(m,0));
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
+        dp[0][0] = (obstacleGrid[0][0] == 1) ? 0 : 1;
+
+        for(int i=1;i<n;i++){
+            if(obstacleGrid[i][0]!=1)
+            dp[i][0]=dp[i-1][0];
+        }
+        for(int j=1;j<m;j++){
+            if(obstacleGrid[0][j]!=1)
+            dp[0][j]=dp[0][j-1];
+        }
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
                 if(obstacleGrid[i][j]==1){
                     dp[i][j]=0;
                 }
-                else if(i==0&&j==0){
-                    dp[i][j]=1;
-                }
                 else{
-                    int up=0;
-                    int left=0;
-                    if(i>0)
-                    up=dp[i-1][j];
-                    if(j>0)
-                    left=dp[i][j-1];
-                    dp[i][j]=up+left;
+                    dp[i][j]=dp[i-1][j]+dp[i][j-1];
                 }
             }
         }
-        return dp[n-1][m-1];
+       return dp[n-1][m-1]; 
     }
 };
