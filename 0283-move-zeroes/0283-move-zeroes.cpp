88ca0e50a9c662d415d84dd077451f0b34472f06
Sheetal Pareek <rajeshpareeksheetal@gmail.com>
2026-08-04 19:29:34 +0530

    Time: 3 ms (14.54%), Space: 25.1 MB (5.68%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>ans;
+        for(int i=0;i<n;i++){
+            if(nums[i]!=0){
+               ans.push_back(nums[i]);
+            }
+        }
+        int m=ans.size();
+        for(int i=1;i<=n-m;i++){
+            ans.push_back(0);
+        }
+        nums=ans;
+    }
+};
