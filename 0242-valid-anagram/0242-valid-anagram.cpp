d1f768a7d0188c7aaaa840ab9072aa6fcfec8b1c
Sheetal Pareek <rajeshpareeksheetal@gmail.com>
2026-08-11 19:04:31 +0530

    Time: 6 ms (22.16%), Space: 9.8 MB (50%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -4,15 +4,12 @@ public:
         int n=s.size();
         int m=t.size();
         if(n!=m)return false;
-        vector<int>freq(26,0);
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
         for(int i=0;i<n;i++){
-            freq[s[i]-'a']++;
-        }
-        for(int i=0;i<n;i++){
-            freq[t[i]-'a']--;
-        }
-        for(int i=0;i<26;i++){
-            if(freq[i]>0)return false;
+            if(s[i]!=t[i]){
+                return false;
+            }
         }
         return true;
     }
